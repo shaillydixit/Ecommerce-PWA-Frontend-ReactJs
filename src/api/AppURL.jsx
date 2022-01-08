98ec361b6e7ad44a7a4ab7c9701c27be0cd6e197
SD <IamCodeX@Shaillys-MacBook-Air.local>
2022-01-08 21:06:01 +0530

    megamenu api consumed

--- src/api/AppURL.jsx
+++ src/api/AppURL.jsx
@@ -2,6 +2,7 @@ class AppURL {
   static BaseURL = 'http://127.0.0.1:8000/api';
   static PostContact = this.BaseURL + '/postcontact';
   static AllSiteInfo = this.BaseURL + '/allsiteinfo';
+  static AllCategoryDeatils = this.BaseURL + '/allcategory';
 }
 
 export default AppURL;
